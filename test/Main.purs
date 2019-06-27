d67a4d2b7f2855c2061fe8425984b26524b16a57
Cole Haus <colehaus@cryptolab.net>
2019-06-26 19:53:41 -0700

    Restructure unit tests to prep for property tests

--- test/Main.purs
+++ test/Main.purs
@@ -19,41 +19,40 @@ import Test.Spec.Runner (run)
 main :: Effect Unit
 main = do
   run [consoleReporter] do
-    let n k v = Tuple k (Tuple k (Set.fromFoldable v ))
-        --       4 - 8
-        --      /     \
-        -- 1 - 2 - 3 - 5 - 7
-        --          \
-        --           6
-        acyclicGraph =
-          Graph.fromMap (
-            Map.fromFoldable
-              [ n 1 [ 2 ]
-              , n 2 [ 3, 4 ]
-              , n 3 [ 5, 6 ]
-              , n 4 [ 8 ]
-              , n 5 [ 7 ]
-              , n 6 [ ]
-              , n 7 [ ]
-              , n 8 [ 5 ]
-              ])
-        --       2 - 4
-        --      / \
-        -- 5 - 1 - 3
-        cyclicGraph =
-          Graph.fromMap (
-            Map.fromFoldable
-              [ n 1 [ 2 ]
-              , n 2 [ 3, 4 ]
-              , n 3 [ 1 ]
-              , n 4 [ ]
-              , n 5 [ 1 ]
-              ])
-    describe "topologicalSort" do
-      it "works for an example" do
+    describe "basic unit tests" do
+      let n k v = Tuple k (Tuple k (Set.fromFoldable v ))
+          --       4 - 8
+          --      /     \
+          -- 1 - 2 - 3 - 5 - 7
+          --          \
+          --           6
+          acyclicGraph =
+            Graph.fromMap (
+              Map.fromFoldable
+                [ n 1 [ 2 ]
+                , n 2 [ 3, 4 ]
+                , n 3 [ 5, 6 ]
+                , n 4 [ 8 ]
+                , n 5 [ 7 ]
+                , n 6 [ ]
+                , n 7 [ ]
+                , n 8 [ 5 ]
+                ])
+          --       2 - 4
+          --      / \
+          -- 5 - 1 - 3
+          cyclicGraph =
+            Graph.fromMap (
+              Map.fromFoldable
+                [ n 1 [ 2 ]
+                , n 2 [ 3, 4 ]
+                , n 3 [ 1 ]
+                , n 4 [ ]
+                , n 5 [ 1 ]
+                ])
+      it "topologicalSort" do
         Graph.topologicalSort acyclicGraph `shouldEqual` List.fromFoldable [ 1, 2, 4, 8, 3, 6, 5, 7 ]
-    describe "insertEdgeWithVertices" do
-      it "works for examples" do
+      it "insertEdgeWithVertices" do
         let t x = Tuple x x
             graph =
               Graph.insertEdgeWithVertices (t 1) (t 2) $
@@ -74,8 +73,7 @@ main = do
                Graph.insertEdgeWithVertices (t 3) (t 1) $
                Graph.empty
         Graph.toMap graph' `shouldEqual` Graph.toMap cyclicGraph
-    describe "descendants" do
-      it "works for examples" do
+      it "descendants" do
         Graph.descendants 1 acyclicGraph `shouldEqual` Set.fromFoldable [ 2, 3, 4, 5, 6, 7, 8 ]
         Graph.descendants 2 acyclicGraph `shouldEqual` Set.fromFoldable [ 3, 4, 5, 6, 7, 8 ]
         Graph.descendants 3 acyclicGraph `shouldEqual` Set.fromFoldable [ 5, 6, 7 ]
@@ -84,8 +82,7 @@ main = do
         Graph.descendants 6 acyclicGraph `shouldEqual` Set.fromFoldable [ ]
         Graph.descendants 7 acyclicGraph `shouldEqual` Set.fromFoldable [ ]
         Graph.descendants 8 acyclicGraph `shouldEqual` Set.fromFoldable [ 5, 7 ]
-    describe "ancestors" do
-      it "works for examples" do
+      it "ancestors" do
         Graph.ancestors 1 acyclicGraph `shouldEqual` Set.fromFoldable [ ]
         Graph.ancestors 2 acyclicGraph `shouldEqual` Set.fromFoldable [ 1 ]
         Graph.ancestors 3 acyclicGraph `shouldEqual` Set.fromFoldable [ 1, 2 ]
@@ -94,21 +91,18 @@ main = do
         Graph.ancestors 6 acyclicGraph `shouldEqual` Set.fromFoldable [ 1, 2, 3 ]
         Graph.ancestors 7 acyclicGraph `shouldEqual` Set.fromFoldable [ 1, 2, 3, 4, 5, 8 ]
         Graph.ancestors 8 acyclicGraph `shouldEqual` Set.fromFoldable [ 1, 2, 4 ]
-    describe "inCycle" do
-      it "works for examples" do
+      it "inCycle" do
         Graph.isInCycle 1 cyclicGraph `shouldEqual` true
         Graph.isInCycle 2 cyclicGraph `shouldEqual` true
         Graph.isInCycle 3 cyclicGraph `shouldEqual` true
         Graph.isInCycle 4 cyclicGraph `shouldEqual` false
         Graph.isInCycle 5 cyclicGraph `shouldEqual` false
-    describe "cyclic" do
-      it "works for examples" do
+      it "cyclic" do
         Graph.isCyclic cyclicGraph `shouldEqual` true
         Graph.isCyclic acyclicGraph `shouldEqual` false
         Graph.isAcyclic cyclicGraph `shouldEqual` false
         Graph.isAcyclic acyclicGraph `shouldEqual` true
-    describe "adjacent" do
-      it "works for examples" do
+      it "adjacent" do
         Graph.adjacent 1 acyclicGraph `shouldEqual` Set.fromFoldable [ 2 ]
         Graph.adjacent 2 acyclicGraph `shouldEqual` Set.fromFoldable [ 1, 3, 4 ]
         Graph.adjacent 3 acyclicGraph `shouldEqual` Set.fromFoldable [ 2, 5, 6 ]
@@ -122,8 +116,7 @@ main = do
         Graph.adjacent 3 cyclicGraph `shouldEqual` Set.fromFoldable [ 1, 2 ]
         Graph.adjacent 4 cyclicGraph `shouldEqual` Set.fromFoldable [ 2 ]
         Graph.adjacent 5 cyclicGraph `shouldEqual` Set.fromFoldable [ 1 ]
-    describe "allPaths" do
-      it "works for examples" do
+      it "allPaths" do
         let nel x = unsafePartial $ fromJust <<< NEL.fromList <<< List.fromFoldable $ x
         Graph.allPaths 2 1 acyclicGraph `shouldEqual` Set.empty
         Graph.allPaths 1 9 acyclicGraph `shouldEqual` Set.empty

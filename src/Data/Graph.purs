4be1d7329da792480e0440aa33f6e35a434fd43e
Cole Haus <colehaus@cryptolab.net>
2019-06-27 11:16:46 -0700

    Add property tests

--- src/Data/Graph.purs
+++ src/Data/Graph.purs
@@ -7,15 +7,22 @@ module Data.Graph
   , toMap
   , empty
   , insertEdge
+  , deleteEdge
   , insertVertex
+  , deleteVertex
   , insertEdgeWithVertices
   , vertices
+  , edges
   , lookup
   , outEdges
   , children
+  , isChildOf
   , descendants
+  , isDescendantOf
   , parents
+  , isParentOf
   , ancestors
+  , isAncestorOf
   , topologicalSort
   , isInCycle
   , isCyclic
@@ -23,8 +30,9 @@ module Data.Graph
   , alterVertex
   , alterEdges
   , adjacent
-  , isAdjacent
+  , areAdjacent
   , areConnected
+  , isValidPath
   , shortestPath
   , allPaths
   ) where
@@ -35,19 +43,22 @@ import Data.Array as Array
 import Data.Bifunctor (lmap, rmap)
 import Data.CatList (CatList)
 import Data.CatList as CL
-import Data.Foldable (class Foldable)
+import Data.Foldable (class Foldable, foldl)
 import Data.Foldable as Foldable
 import Data.List (List(..))
-import Data.List.NonEmpty (NonEmptyList)
+import Data.List as L
+import Data.List.NonEmpty (NonEmptyList(..))
 import Data.List.NonEmpty as NEL
 import Data.List.Types (nelCons)
 import Data.Map (Map)
 import Data.Map as M
-import Data.Maybe (Maybe(..), isJust, maybe)
+import Data.Maybe (Maybe(..), fromJust, isJust, maybe)
+import Data.NonEmpty ((:|))
 import Data.Set (Set)
 import Data.Set as S
 import Data.Set as Set
 import Data.Tuple (Tuple(..), fst, snd, uncurry)
+import Partial.Unsafe (unsafePartialBecause)
 
 -- | A graph with vertices of type `v`.
 -- |
@@ -62,21 +73,43 @@ empty :: forall k v. Graph k v
 empty = Graph M.empty
 
 -- | Insert an edge from the start key to the end key.
-insertEdge :: forall k v. Ord k => k -> k -> Graph k v -> Graph k v
+-- |
+-- | Does nothing if the edge is already present.
+-- | Returns `Nothing` iff either key is absent from the graph.
+insertEdge :: forall k v. Ord k => k -> k -> Graph k v -> Maybe (Graph k v)
 insertEdge from to (Graph g) =
-  Graph $ M.alter (map (rmap (S.insert to))) from g
+  if from `Set.member` keys && to `Set.member` keys
+  then Just $ Graph $ M.alter (map (rmap (S.insert to))) from g
+  else Nothing
+  where
+    keys = M.keys g
+
+-- | Remove edge from the start key to the end key.
+-- |
+-- | Does nothing if the edge isn't present.
+deleteEdge :: forall k v. Ord k => k -> k -> Graph k v -> Graph k v
+deleteEdge from to (Graph g) =
+  Graph $ M.alter (map (rmap (S.delete to))) from g
 
 -- | Insert a vertex into the graph.
 -- |
 -- | If the key already exists, replaces the existing value and
--- |preserves existing edges.
+-- | preserves existing edges.
 insertVertex :: forall k v. Ord k => k -> v -> Graph k v -> Graph k v
-insertVertex k v (Graph g) = Graph $ M.insertWith (\(Tuple _ ks) _ -> Tuple v ks) k (Tuple v mempty) g
+insertVertex k v (Graph g) =
+  Graph $ M.insertWith (\(Tuple _ ks) _ -> Tuple v ks) k (Tuple v mempty) g
+
+-- | Remove the given vertex from the graph.
+-- |
+-- | Does nothing if the key isn't present.
+deleteVertex :: forall k v. Ord k => k -> Graph k v -> Graph k v
+deleteVertex k (Graph g) = Graph $ M.delete k g
 
 -- | Insert two vertices and connect them.
 insertEdgeWithVertices :: forall k v. Ord k => Tuple k v -> Tuple k v -> Graph k v -> Graph k v
-insertEdgeWithVertices from@(Tuple fromKey _) to@(Tuple toKey _) =
-  insertEdge fromKey toKey <<< uncurry insertVertex from <<< uncurry insertVertex to
+insertEdgeWithVertices from@(Tuple fromKey _) to@(Tuple toKey _) g =
+  unsafePartialBecause "Just inserted vertices" $ fromJust <<<
+  insertEdge fromKey toKey <<< uncurry insertVertex from <<< uncurry insertVertex to $ g
 
 -- | Unfold a `Graph` from a collection of keys and functions which label keys
 -- | and specify out-edges.
@@ -90,9 +123,9 @@ unfoldGraph
   -> (k -> v)
   -> (k -> out k)
   -> Graph k v
-unfoldGraph ks label edges =
+unfoldGraph ks label edges' =
   Graph (M.fromFoldable (map (\k ->
-            Tuple k (Tuple (label k) (S.fromFoldable (edges k)))) ks))
+    Tuple k (Tuple (label k) (S.fromFoldable (edges' k)))) ks))
 
 -- | Create a `Graph` from a `Map` which maps vertices to their labels and
 -- | outgoing edges.
@@ -104,16 +137,26 @@ fromMap = Graph
 toMap :: forall k v. Graph k v -> Map k (Tuple v (Set k))
 toMap (Graph g) = g
 
--- | Check if the first key is adjacent to the second.
-isAdjacent :: forall k v. Ord k => k -> k -> Graph k v -> Boolean
-isAdjacent k1 k2 g = k1 `Set.member` adjacent k2 g
+-- | Check if the keys are adjacent.
+-- |
+-- | Returns `false` if either key is absent from the graph.
+areAdjacent :: forall k v. Ord k => Graph k v -> k -> k -> Boolean
+areAdjacent g k1 k2 = k1 `Set.member` adjacent k2 g
 
 -- | Find all keys adjacent to given key.
+-- |
+-- | Returns `mempty` if the key is absent from the graph.
 adjacent :: forall k v. Ord k => k -> Graph k v -> Set k
 adjacent k g = children k g `Set.union` parents k g
 
+isValidPath :: forall k v. Ord k => Graph k v -> NonEmptyList k -> Boolean
+isValidPath g (NonEmptyList (k :| ks)) =
+  fst $ foldl (\(Tuple b p) c -> Tuple (p `isParentOf g` c) c) (Tuple true k) ks
+
 -- | Returns shortest path between start and end key if it exists.
 -- |
+-- | Returns `Nothing` if there's no path between the keys or
+-- | if the keys aren't present in the graph.
 -- | Cyclic graphs may return bottom.
 shortestPath :: forall k v. Ord k => k -> k -> Graph k v -> Maybe (NonEmptyList k)
 shortestPath start end g =
@@ -126,6 +169,7 @@ shortestPath start end g =
 -- | Returns shortest path between start and end key if it exists.
 -- |
 -- | Cyclic graphs may return bottom.
+-- | Returns `mempty` if either key is absent from the graph.
 allPaths :: forall k v. Ord k => k -> k -> Graph k v -> Set (NonEmptyList k)
 allPaths start end g = Set.map NEL.reverse $ go Nothing start
   where
@@ -141,12 +185,18 @@ allPaths start end g = Set.map NEL.reverse $ go Nothing start
         hist' = maybe (pure k) (nelCons k) hist
 
 -- | Checks if there's a directed path between the start and end key.
-areConnected :: forall k v. Ord k => k -> k -> Graph k v -> Boolean
-areConnected start end g = isJust $ shortestPath start end g
+areConnected :: forall k v. Ord k => Graph k v -> k -> k -> Boolean
+areConnected g start end = isJust $ shortestPath start end g
 
 -- | List all vertices in a graph.
 vertices :: forall k v. Graph k v -> List v
 vertices (Graph g) = map fst (M.values g)
+
+-- | List all edges in a graph.
+edges :: forall k v. Ord k => Graph k v -> Set (Tuple k k)
+edges (Graph g) =
+  Set.fromFoldable $ (\(Tuple k x) -> map (Tuple k) <<< L.fromFoldable <<< snd $ x) =<<
+  M.toUnfoldable g
 
 -- | Lookup a vertex by its key.
 lookup :: forall k v. Ord k => k -> Graph k v -> Maybe v
@@ -157,12 +207,19 @@ outEdges :: forall k v. Ord k => k -> Graph k v -> Maybe (Set k)
 outEdges k (Graph g) = map snd (M.lookup k g)
 
 -- | Returns immediate ancestors of given key.
+-- |
+-- | Returns `mempty` if the queried key is absent from the graph.
 parents :: forall k v. Ord k => k -> Graph k v -> Set k
 parents k (Graph g) = M.keys <<< M.filter (Foldable.elem k <<< snd) $ g
 
+-- | Returns `false` if either key is absent from the graph.
+isParentOf :: forall k v. Ord k => Graph k v -> k -> k -> Boolean
+isParentOf g k1 k2 = k1 `Set.member` parents k2 g
+
 -- | Returns all ancestors of given key.
 -- |
--- | Will return bottom if `k` is in cycle.
+-- | Returns bottom if `k` is in cycle.
+-- | Returns `mempty` if the queried key is absent from the graph.
 ancestors :: forall k v. Ord k => k -> Graph k v -> Set k
 ancestors k' g = go k'
   where
@@ -170,19 +227,34 @@ ancestors k' g = go k'
      where
        da = parents k g
 
+-- | Returns `false` if either key is absent from the graph.
+isAncestorOf :: forall k v. Ord k => Graph k v -> k -> k -> Boolean
+isAncestorOf g k1 k2 = k1 `Set.member` ancestors k2 g
+
 -- | Returns immediate descendants of given key.
+-- |
+-- | Returns `mempty` if the queried key is absent from the graph.
 children :: forall k v. Ord k => k -> Graph k v -> Set k
 children k (Graph g) = maybe mempty (Set.fromFoldable <<< snd) <<< M.lookup k $ g
 
+-- | Returns `false` if either key is absent from the graph.
+isChildOf :: forall k v. Ord k => Graph k v -> k -> k -> Boolean
+isChildOf g k1 k2 = k1 `Set.member` children k2 g
+
 -- | Returns all descendants of given key.
 -- |
--- | Will return bottom if `k` is in cycle.
+-- | Returns bottom if `k` is in cycle.
+-- | Returns `mempty` if the queried key is absent from the graph.
 descendants :: forall k v. Ord k => k -> Graph k v -> Set k
 descendants k' g = go k'
   where
    go k = Set.unions $ Set.insert dd $ Set.map go dd
      where
        dd = children k g
+
+-- | Returns `false` if either key is absent from the graph.
+isDescendantOf :: forall k v. Ord k => Graph k v -> k -> k -> Boolean
+isDescendantOf g k1 k2 = k1 `Set.member` descendants k2 g
 
 -- | Checks if given key is part of a cycle.
 isInCycle :: forall k v. Ord k => k -> Graph k v -> Boolean
--- test/Main.purs
+++ test/Main.purs
@@ -2,19 +2,37 @@ module Test.Main where
 
 import Prelude
 
+import Control.Algebra.Properties as Props
+import Data.Foldable (foldr)
+import Data.Foldable as Foldable
+import Data.Function (on)
+import Data.Generic.Rep (class Generic)
+import Data.Graph (Graph)
 import Data.Graph as Graph
+import Data.HeytingAlgebra (implies)
 import Data.List as List
 import Data.List.NonEmpty as NEL
 import Data.Map as Map
-import Data.Maybe (fromJust)
+import Data.Maybe (Maybe(..), fromJust, maybe)
+import Data.Newtype (class Newtype, over, un)
+import Data.Set (Set)
 import Data.Set as Set
-import Data.Tuple (Tuple(..))
+import Data.Tuple (Tuple(..), fst, uncurry)
 import Effect (Effect)
-import Partial.Unsafe (unsafePartial)
+import Effect.Aff (Aff)
+import Partial.Unsafe (unsafePartial, unsafePartialBecause)
+import Test.QuickCheck (arbitrary, (<=?), (<?>), (===))
+import Test.QuickCheck as QuickCheck
+import Test.QuickCheck.Gen (Gen, suchThat)
+import Test.QuickCheck.Gen as Gen
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.QuickCheck (quickCheck')
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
+
+quickCheck'' :: forall t2. QuickCheck.Testable t2 => t2 -> Aff Unit
+quickCheck'' = quickCheck' 2000
 
 main :: Effect Unit
 main = do
@@ -64,7 +82,7 @@ main = do
               Graph.insertEdgeWithVertices (t 3) (t 5) $
               Graph.insertEdgeWithVertices (t 3) (t 6) $
               Graph.empty
-        Graph.toMap graph `shouldEqual` Graph.toMap acyclicGraph
+        G graph `shouldEqual` G acyclicGraph
         let graph' =
                Graph.insertEdgeWithVertices (t 5) (t 1) $
                Graph.insertEdgeWithVertices (t 1) (t 2) $
@@ -72,7 +90,7 @@ main = do
                Graph.insertEdgeWithVertices (t 2) (t 3) $
                Graph.insertEdgeWithVertices (t 3) (t 1) $
                Graph.empty
-        Graph.toMap graph' `shouldEqual` Graph.toMap cyclicGraph
+        G graph' `shouldEqual` G cyclicGraph
       it "descendants" do
         Graph.descendants 1 acyclicGraph `shouldEqual` Set.fromFoldable [ 2, 3, 4, 5, 6, 7, 8 ]
         Graph.descendants 2 acyclicGraph `shouldEqual` Set.fromFoldable [ 3, 4, 5, 6, 7, 8 ]
@@ -127,3 +145,255 @@ main = do
         Graph.allPaths 1 8 acyclicGraph `shouldEqual` Set.singleton (nel [ 1, 2, 4, 8 ])
         Graph.allPaths 2 6 acyclicGraph `shouldEqual` Set.singleton (nel [ 2, 3, 6 ])
         Graph.allPaths 5 3 cyclicGraph `shouldEqual` Set.singleton (nel [ 5, 1, 2, 3 ])
+    describe "relationships" do
+      it "shortestPath returns a valid path" do
+        quickCheck'' ado
+          Tuple k1 k2 <- arbitrary
+          g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in (Foldable.all (Graph.isValidPath g) <<< Graph.shortestPath k1 k2 $ g)
+      it "isParentOf implies isAncestorOf" do
+        quickCheck'' ado
+          Tuple k1 k2 <- arbitrary
+          g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in
+            (k1 `Graph.isParentOf g` k2) `implies` (k1 `Graph.isAncestorOf g` k2) <?>
+            show (Tuple (Tuple k1 k2) (G g))
+      it "isChildOf implies isDescendantOf" do
+        quickCheck'' ado
+          Tuple k1 k2 <- arbitrary
+          g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in (k1 `Graph.isChildOf g` k2) `implies` (k1 `Graph.isDescendantOf g` k2)
+      it "isAncestorOf and isDescendantOf are symmetric" do
+        quickCheck'' ado
+          Tuple k1 k2 <- arbitrary
+          g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in k1 `Graph.isAncestorOf g` k2 === k2 `Graph.isDescendantOf g` k1
+      it "isParentOf and isChildOf are symmetric" do
+        quickCheck'' ado
+          Tuple k1 k2 <- arbitrary
+          g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in k1 `Graph.isParentOf g` k2 === k2 `Graph.isChildOf g` k1
+      it "insertVertex and deleteVertex are opposites" do
+        quickCheck'' do
+          g' <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          Tuple k v <- arbitrary
+          let g = Graph.deleteVertex k g'
+          pure $ (G <<< Graph.deleteVertex k <<< Graph.insertVertex k v $ g) === G g
+      it "insertVertex and vertices cohere" do
+        quickCheck'' ado
+          Tuple k v <- arbitrary
+          g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in
+            let
+              len = List.length (Graph.vertices g)
+              len' = List.length (Graph.vertices <<< Graph.insertVertex k v $ g)
+            in
+            len == len' || len + 1 == len'
+      it "deleteVertex and vertices cohere" do
+        quickCheck'' ado
+          k <- arbitrary
+          g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in
+            let
+              len = List.length (Graph.vertices g)
+              len' = List.length (Graph.vertices <<< Graph.deleteVertex k $ g)
+            in
+            len == len' || len - 1 == len'
+      it "deleteEdge and edges cohere" do
+        quickCheck'' ado
+          Tuple k1 k2 <- arbitrary
+          g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in Set.delete (Tuple k1 k2) (Graph.edges g) === Graph.edges (Graph.deleteEdge k1 k2 g)
+      it "insertEdgeWithVertices and edges cohere" do
+        quickCheck'' ado
+          Tuple k1 k2 <- arbitrary
+          g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+          in
+            Set.insert (Tuple (fst k1) (fst k2)) (Graph.edges g) ===
+            Graph.edges (Graph.insertEdgeWithVertices k1 k2 g)
+      it "insertEdge and deleteEdge are opposites" do
+        let fromJust' x = unsafePartialBecause "genGraphAndEdge ensures keys present" $ fromJust x
+        quickCheck'' do
+          Tuple g' (Tuple k1 k2) <- genGraphAndEdge arbitrary arbitrary :: Gen (Tuple (Graph SmallInt SmallInt) _)
+          let g = fromJust' <<< Graph.insertEdge k1 k2 $ g'
+          pure $ (map G <<< Graph.insertEdge k1 k2 <<< Graph.deleteEdge k1 k2 $ g) === Just (G g)
+        quickCheck'' do
+          Tuple g' (Tuple k1 k2) <- genGraphAndEdge arbitrary arbitrary :: Gen (Tuple (Graph SmallInt SmallInt) _)
+          let g = Graph.deleteEdge k1 k2 g'
+          pure $ G (Graph.deleteEdge k1 k2 <<< fromJust' <<< Graph.insertEdge k1 k2 $ g) === G g
+    describe "property tests" do
+      describe "areConnected" do
+        it "is transitive" do
+          quickCheck'' ado
+            Tuple k1 (Tuple k2 k3) <- arbitrary
+            g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.transitive (Graph.areConnected g) k1 k2 k3
+      describe "areAdjacent" do
+        it "is antireflexive" do
+          quickCheck'' ado
+            k <- arbitrary
+            g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.antireflexive (Graph.areAdjacent g) k || Graph.isCyclic g
+        it "is commutative" do
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.commutative (Graph.areAdjacent g) k1 k2
+      describe "isDescendantOf" do
+        it "is transitive" do
+          quickCheck'' ado
+            Tuple k1 (Tuple k2 k3) <- arbitrary
+            g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.transitive (Graph.isDescendantOf g) k1 k2 k3
+        it "is asymmetric" do
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.asymmetric (Graph.isDescendantOf g) k1 k2
+      describe "isChildOf" do
+        it "is asymmetric" do
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.asymmetric (Graph.isChildOf g) k1 k2 || Graph.isCyclic g
+      describe "isAncestorOf" do
+        it "is transitive" do
+          quickCheck'' ado
+            Tuple k1 (Tuple k2 k3) <- arbitrary
+            g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.transitive (Graph.isAncestorOf g) k1 k2 k3
+        it "is asymmetric" do
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.asymmetric (Graph.isAncestorOf g) k1 k2
+      describe "isParentOf" do
+        it "is asymmetric" do
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.asymmetric (Graph.isParentOf g) k1 k2 || Graph.isCyclic g
+      describe "deleteVertex" do
+        it "deleting all vertices results in empty graph" do
+          quickCheck'' ado
+            g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in G (foldr Graph.deleteVertex g <<< Map.keys <<< Graph.toMap $ g) == G Graph.empty
+        it "is idempotent" do
+          quickCheck'' ado
+            k <- arbitrary
+            g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in idempotent (over G (Graph.deleteVertex k)) (G g)
+        it "is congruent" do
+          quickCheck'' ado
+            k <- arbitrary
+            g1 <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            g2 <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.congruent (over G (Graph.deleteVertex k)) (G g1) (G g2)
+      describe "insertVertex" do
+        it "is idempotent" do
+          quickCheck'' ado
+            Tuple k v <- arbitrary
+            g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in idempotent (over G (Graph.insertVertex k v)) (G g)
+        it "is congruent" do
+          quickCheck'' ado
+            Tuple k v <- arbitrary
+            g1 <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            g2 <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.congruent (over G (Graph.insertVertex k v)) (G g1) (G g2)
+      describe "deleteEdge" do
+        it "is congruent" do
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            g1 <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            g2 <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Props.congruent (over G (Graph.deleteEdge k1 k2)) (G g1) (G g2)
+        it "is idempotent" do
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            g <- genGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in idempotent (over G (Graph.deleteEdge k1 k2)) (G g)
+      describe "insertEdge" do
+        it "is idempotent" do
+          let fromJust' x = unsafePartialBecause "genGraphAndEdge ensures keys present" $ fromJust x
+          quickCheck'' ado
+            Tuple g (Tuple k1 k2) <- genGraphAndEdge arbitrary arbitrary :: Gen (Tuple (Graph SmallInt SmallInt) _)
+            in idempotent (G <<< fromJust' <<< Graph.insertEdge k1 k2 <<< un G) (G g)
+      describe "shortestPath" do
+        it "never gets longer with edge additions" do
+          let fromJust' x = unsafePartialBecause "genGraphAndEdge ensures keys present" $ fromJust x
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            Tuple g (Tuple k3 k4) <- genAcyclicGraphAndEdge arbitrary arbitrary :: Gen (_ (Graph SmallInt SmallInt) _)
+            in
+              Graph.isCyclic (fromJust' $ Graph.insertEdge k3 k4 g) ||
+                (maybe 9999 NEL.length $ Graph.shortestPath k1 k2 g) >=
+                (maybe 9999 NEL.length $ Graph.shortestPath k1 k2 =<< Graph.insertEdge k3 k4 g)
+        it "never gets shorter with edge deletions" do
+          quickCheck'' ado
+            Tuple k1 k2 <- arbitrary
+            Tuple k3 k4 <- arbitrary
+            g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in
+              (NEL.length <$> Graph.shortestPath k1 k2 g) <=?
+              (NEL.length <$> Graph.shortestPath k1 k2 (Graph.deleteEdge k3 k4 g))
+      describe "topologicalSort" do
+        it "returns all vertices" do
+          quickCheck'' ado
+            g <- genAcyclicGraph arbitrary arbitrary :: Gen (Graph SmallInt SmallInt)
+            in Set.fromFoldable (Graph.topologicalSort g) === Map.keys (Graph.toMap g)
+
+-- TODO: test alterVertex and alterEdges
+
+genAcyclicGraphAndEdge :: forall k v. Ord k => Gen k -> Gen v -> Gen (Tuple (Graph k v) (Tuple k k))
+genAcyclicGraphAndEdge genK genV = suchThat (genGraphAndEdge genK genV) (Graph.isAcyclic <<< fst)
+
+genGraphAndEdge :: forall k v. Ord k => Gen k -> Gen v -> Gen (Tuple (Graph k v) (Tuple k k))
+genGraphAndEdge genK genV = ado
+    n1 <- Tuple <$> genK <*> genV
+    n2 <- Tuple <$> genK <*> genV
+    g <- genGraph genK genV
+    in Tuple (uncurry Graph.insertVertex n2 <<< uncurry Graph.insertVertex n1 $ g) (Tuple (fst n1) (fst n2))
+
+genAcyclicGraph :: forall k v. Ord k => Gen k -> Gen v -> Gen (Graph k v)
+genAcyclicGraph genK genV = suchThat (genGraph genK genV) Graph.isAcyclic
+
+genGraph :: forall k v. Ord k => Gen k -> Gen v -> Gen (Graph k v)
+genGraph genK genV = ado
+  l <- Gen.arrayOf genElement
+  in Graph.fromMap <<< Map.fromFoldable $ l
+    where
+      genElement :: Gen (Tuple k (Tuple v (Set k)))
+      genElement = ado
+        k <- genK
+        v <- genV
+        ks <- genSet genK
+        in (Tuple k (Tuple v ks))
+
+genSet :: forall a. Ord a => Gen a -> Gen (Set a)
+genSet genA = Set.fromFoldable <$> Gen.arrayOf genA
+
+-- The implementation in the property library is maybe wrong?
+idempotent :: forall a. Eq a => (a -> a) -> a -> Boolean
+idempotent f a = f (f a) == f a
+
+-- Convenient for use with specs
+newtype GraphWrapper k v = G (Graph k v)
+derive instance newtypeStringGraph :: Newtype (GraphWrapper k v) _
+instance eqStringGraph :: (Eq k, Eq v) => Eq (GraphWrapper k v) where
+  eq = eq `on` (Graph.toMap <<< un G)
+instance showStringGraph :: (Show k, Show v) => Show (GraphWrapper k v) where
+  show = show <<< Graph.toMap <<< un G
+
+-- We want to make the graph denser so that we actually have edges and cycles
+newtype SmallInt = MkSmallInt Int
+derive instance eqSmallInt :: Eq SmallInt
+derive instance ordSmallInt :: Ord SmallInt
+derive instance genericSmallInt :: Generic SmallInt _
+derive instance newtypeSmallInt :: Newtype SmallInt _
+derive newtype instance semiringSmallInt :: Semiring SmallInt
+derive newtype instance ringSmallInt :: Ring SmallInt
+instance showSmallInt :: Show SmallInt where
+  show (MkSmallInt i) = show i
+instance arbitrarySmallInt :: QuickCheck.Arbitrary SmallInt where
+  arbitrary = MkSmallInt <$> Gen.chooseInt 1 20
